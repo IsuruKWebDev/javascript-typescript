04662baf0c0c272115a8f0ddd52294a212e3b687
isuru kularathna <isurujr.online@gmail.com>
2024-01-18 21:26:44 +0530

    add js and html

--- hello.js
+++ hello.js
@@ -1,1 +1,24 @@
 console.log("https://www.w3schools.com/");
+
+let carName = "Volvo";
+
+const txtFunction = () => {
+
+    let result = generateRandomString();
+    document.getElementById("demo").innerHTML = result;
+}
+
+function alertDisplay() {
+    window.alert("Alllerrrrrrrt...........");
+}
+
+function generateRandomString() {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    let res = '';
+    for (let i = 0; i < characters.length; i++) {
+        res += characters.charAt(Math.floor(Math.random() * 20));
+    }
+
+    return res;
+}
